13bddd9095f3c6a4bb48e64ee670f72ec121f2fa
Patti Philippe <pattiphilippe1@gmail.com>
2021-07-16 15:26:11 +0200

    Event Detail - Start of Session players times

--- src/main/java/raceroom/calculator/controllers/EventsController.java
+++ src/main/java/raceroom/calculator/controllers/EventsController.java
@@ -1,5 +1,6 @@
 package raceroom.calculator.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import raceroom.calculator.repositories.EventEntity;
 import raceroom.calculator.repositories.EventRepository;
+import raceroom.calculator.repositories.EventResultsRepository;
+import raceroom.calculator.repositories.PlayerEntity;
+import raceroom.calculator.repositories.PlayerRepository;
+import raceroom.calculator.repositories.SessionRepository;
 
 @Controller
 public class EventsController {
 
     @Autowired
     private EventRepository eventRepository;
+    @Autowired 
+    private EventResultsRepository eventResultsRepository;
+    @Autowired
+    private SessionRepository sessionRepository;
+    @Autowired
+    private PlayerRepository playerRepository;
     
     @GetMapping("/events")
     public String events(Model model){
@@ -31,6 +42,16 @@ public class EventsController {
         }
 
         model.addAttribute("event", event.get());
+        //TODO search on event id, name, or whatever
+        model.addAttribute("results", eventResultsRepository.findAll());
+        model.addAttribute("sessions", sessionRepository.getSessionsByEventId(id));
         return "eventDetail";
+    }
+
+    @GetMapping("/events/{id}/{sessionType}")
+    public String eventSessionDetail(@PathVariable("id") Long id, @PathVariable("sessionType") String sessionType, Model model){
+        List<PlayerEntity> players = playerRepository.getPlayersByEventIdAndSessionTypeOrderByPositionAsc(id, sessionType);
+        model.addAttribute("players", players);
+        return "eventSessionDetail";
     }
 }
