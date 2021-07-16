8c2ec9128451f85a0a04995a107e29f24f07a35f
Patti Philippe <pattiphilippe1@gmail.com>
2021-07-16 12:16:42 +0200

    Home + Players first pages

---
+++ src/main/java/raceroom/calculator/controllers/PlayersController.java
@@ -0,0 +1,21 @@
+package raceroom.calculator.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import raceroom.calculator.repositories.PlayerRepository;
+
+@Controller
+public class PlayersController {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @GetMapping("/players")
+    public String home(Model model){
+        model.addAttribute("players", playerRepository.findAll());
+        return "players";
+    }
+}
